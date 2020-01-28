884180578b24f07e09b6bee559bb7915eee45442
PASSELEUR Cyril (s) <cyril.passeleur@student.ehb.be>
2020-01-28 12:26:55 +0100

    Animations&characters

---
+++ unity stickfight/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake(){
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void move(){
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 temp = transform.position;
+        if(h > 0 ){
+            temp.x += speed* Time.deltaTime;
+        }else if(h < 0){
+            temp.x -= speed * Time.deltaTime;
+        }
+        transform.position = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move(); 
+    }
+}
