b8fbb11ae69c7f8a8cb8b00037f22a165b01a5a2
PASSELEUR Cyril (s) <cyril.passeleur@student.ehb.be>
2020-01-29 14:40:37 +0100

    powerup

---
+++ unity stickfight/Assets/Scripts/collision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(8,9);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.name == "snowBall"){
+            Debug.Log("GOED");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
