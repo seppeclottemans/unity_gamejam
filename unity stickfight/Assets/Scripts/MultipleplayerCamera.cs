57fcec6cfff48b1b5dab7fb9ddbdac69ebb4f710
CLOTTEMANS Seppe (s) <seppe.clottemans@student.ehb.be>
2020-01-29 11:26:54 +0100

    camera

---
+++ unity stickfight/Assets/Scripts/MultipleplayerCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleplayerCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset;
+ 
+    void LateUpdate(){
+
+        
+        Vector3 centerpoit = GetCenterPoint();
+
+        Vector3 newposition = centerpoit + offset;
+
+        transform.position = newposition;
+    }
+
+    Vector3 GetCenterPoint(){
+        if(targets.Count == 1){
+            return targets[0].position;
+        }
+
+        var bounds =  new Bounds(targets[0].position, Vector3.zero);
+        for(int i = 0; i < targets.Count; i++){
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+}
