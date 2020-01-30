b7da558d88f018521cb89a71dbff92ee19cbc4cb
CLOTTEMANS Seppe (s) <seppe.clottemans@student.ehb.be>
2020-01-30 10:49:02 +0100

    Main menu

---
+++ unity stickfight/Assets/mainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void PlayGame(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
